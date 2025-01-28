8e26b6dd9a2dfdfb3a5ec00807ba54926d6e8414
seonzone <tjdwhd2247@naver.com>
2025-01-28 22:38:26 +0900

    feat: 장소 select box 방식으로 변경

---
+++ src/LocationSelect.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LocationSelect = ({onChange}) => {
+    return (
+        <div className="select-container">
+            <select
+                className="form-select"
+                onChange={onChange}
+            >
+                <option value="">장소를 선택하세요</option>
+                <option value="신한은행">신한은행</option>
+                <option value="새마을금고">새마을금고</option>
+                <option value="국민은행">국민은행</option>
+            </select>
+        </div>
+    );
+};
+
+export default LocationSelect;
--- src/OutWork.jsx
+++ src/OutWork.jsx
@@ -3,136 +3,137 @@ import UserSelect from "./UserSelect";
 import UpdateStatus from "./UpdateStatus.jsx";
 import UserSelectCondition from "./UserSelectCondition.jsx";
 import api from "./api/axios.js";
+import LocationSelect from "./LocationSelect.jsx";
 
 
-function OutWork() {
+    function OutWork() {
     const [requestUser, setRequestUser] = useState([]);
     const [approverUser, setApproverUser] = useState([]);
     const [requestList, setRequestList] = useState([]);
     const [statusList, setStatusList] = useState([]);
     const [formData, setFormData] = useState({
-        requestUserId: '',
-        approverUserId: '',
-        location: ''
-    });
+    requestUserId: '',
+    approverUserId: '',
+    location: '',
+});
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const [reqUserResponse, approverUserRequest, requestUserList,statusList] = await Promise.all([
-                    api.post('/outwork/list/reqUser'),
-                    api.post('/outwork/list/approverUser'),
-                    api.post('/outwork/list/requestList'),
-                    api.post('/outwork/list/statusList'),
-                ]);
-                setRequestUser(reqUserResponse.data);
-                setApproverUser(approverUserRequest.data);
-                setRequestList(requestUserList.data);
-                setStatusList(statusList.data);
-            } catch (error) {
-                console.log("직원 목록을 불러오는중 에러 발생!: ", error)
-            }
-        };
-        fetchUsers();
-    }, []);
+    const fetchUsers = async () => {
+    try {
+    const [reqUserResponse, approverUserRequest, requestUserList, statusList] = await Promise.all([
+    api.post('/outwork/list/reqUser'),
+    api.post('/outwork/list/approverUser'),
+    api.post('/outwork/list/requestList'),
+    api.post('/outwork/list/statusList'),
+    ]);
+    setRequestUser(reqUserResponse.data);
+    setApproverUser(approverUserRequest.data);
+    setRequestList(requestUserList.data);
+    setStatusList(statusList.data);
+} catch (error) {
+    console.log("직원 목록을 불러오는중 에러 발생!: ", error)
+}
+};
+    fetchUsers();
+}, []);
 
     //직원 선택 핸들러
     const userSelectHandler = (target) => (e) => {
-        setFormData({
-            ...formData,
-            [target]: e.target.value
-        });
-    }
+    setFormData({
+    ...formData,
+    [target]: e.target.value
+});
+}
+
     // 장소 선택 핸들러
-    const locationHandler = (e) => {
-        setFormData({
-            ...formData,
-            location: e.target.value
-        })
-    }
+    const locationSelectHandler = (target) => (e) => {
+    setFormData({
+    ...formData,
+    [target]: e.target.value
+})
+}
 
     const requestOutWork = async () => {
-        try {
-            if (!confirm('외근을 신청 하시겠습니까?')) return false;
-            const response = await api.post('/outwork/request', formData);
-            if (response.data.code !== 200) {
-                alert(response.data.message);
-                return false;
-            }
-            console.log('외근 신청 성공', response);
-            alert('외근이 신청되었습니다.');
-            refreshList();
-            setFormData({
-                requestUserId: '',
-                approverUserId: '',
-                location: ''
-            });
-        } catch (error) {
-            alert('외근 신청이 실패 하였습니다 ', error);
-            console.log('외근 신청 실패', error);
-        }
-    }
+    try {
+    if (!confirm('외근을 신청 하시겠습니까?')) return false;
+    const response = await api.post('/outwork/request', formData);
+    if (response.data.code !== 200) {
+    alert(response.data.message);
+    return false;
+}
+    console.log('외근 신청 성공', response);
+    alert('외근이 신청되었습니다.');
+    refreshList();
+    setFormData({
+    requestUserId: '',
+    approverUserId: '',
+    location: ''
+});
+} catch (error) {
+    alert('외근 신청이 실패 하였습니다 ', error);
+    console.log('외근 신청 실패', error);
+}
+}
 
     //승인 목록 리프레시
     const refreshList = async (data) => {
-        try {
-            const requestListReload = await api.post('/outwork/list/requestList',data);
-            setRequestList(requestListReload.data);
-        } catch (error) {
-            console.log("승인요청 목록을 불러오는중 에러 발생!: ", error)
-        }
-    }
+    try {
+    const requestListReload = await api.post('/outwork/list/requestList', data);
+    setRequestList(requestListReload.data);
+} catch (error) {
+    console.log("승인요청 목록을 불러오는중 에러 발생!: ", error)
+}
+}
 
 
     return (
-        <div className="outwork-container">
-            <div className="outwork-card">
-                <h3 className="outwork-title">어다인 외근 신청</h3>
-                <div className="form-row">
-                    <UserSelect
-                        label="요청자"
-                        users={requestUser}
-                        onChange={userSelectHandler('requestUserId')}
-                        value={formData.requestUserId}
-                    />
-                </div>
-                <div className="form-row">
-                    <UserSelect
-                        label="승인자"
-                        users={approverUser}
-                        onChange={userSelectHandler('approverUserId')}
-                        value={formData.approverUserId}
-                    />
-                </div>
-                <div className="location-input-group">
-                    <input
-                        className="form-control"
-                        type="text"
-                        onChange={locationHandler}
-                        placeholder="장소"
-                        value={formData.location}
-                    />
-                    <button className="btn btn-primary" onClick={requestOutWork}>
-                        신청
-                    </button>
-                </div>
-                <div className="location-input-group">
-                    <UserSelectCondition
-                        requestUsers={requestUser}
-                        approverUsers={approverUser}
-                        statusList={statusList}
-                        onSearchConditionChange={refreshList}
-                    />
-                </div>
-                <div className="table-container">
-                    <UpdateStatus
-                        users={requestList}
-                        refreshList={refreshList}
-                    />
-                </div>
-            </div>
-        </div>
-    );
+    <div className="outwork-container">
+    <div className="outwork-card">
+    <h3 className="outwork-title">어다인 외근 신청</h3>
+    <div className="form-row">
+    <UserSelect
+    label="요청자"
+    users={requestUser}
+      onChange={userSelectHandler('requestUserId')}
+      value={formData.requestUserId}
+/>
+</div>
+<div className="form-row">
+    <UserSelect
+        label="승인자"
+        users={approverUser}
+        onChange={userSelectHandler('approverUserId')}
+        value={formData.approverUserId}
+    />
+</div>
+<div className="form-row">
+    <LocationSelect
+        onChange={locationSelectHandler('location')}
+    />
+</div>
+<div className="location-input-group">
+    <button className="btn btn-primary" style={{width: '100%'}} onClick={requestOutWork}>
+        신청
+    </button>
+</div>
+<div className="location-input-group">
+    <UserSelectCondition
+        requestUsers={requestUser}
+        approverUsers={approverUser}
+        statusList={statusList}
+        onSearchConditionChange={refreshList}
+    />
+</div>
+<div className="table-container">
+    <UpdateStatus
+        users={requestList}
+        refreshList={refreshList}
+    />
+</div>
+</div>
+</div>
+)
+;
 }
 
 export default OutWork
