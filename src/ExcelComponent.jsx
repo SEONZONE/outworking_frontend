a664be44d6292e62fad0b6a010f5b26fb70ad3ba
seonzone <tjdwhd2247@naver.com>
2025-02-05 10:39:40 +0900

    fix: 요청날짜 컬럼명 수정 -> 처리일시

---
+++ src/ExcelComponent.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {Button} from 'react-bootstrap';
+import XLSX from 'xlsx-js-style';
+
+const ExcelComponent = ({data}) => {
+    const excelDownLoad = async () => {
+        try {
+            const wb = XLSX.utils.book_new()
+            const headerStyle = {
+                font: {
+                    bold: true,
+                    color: {rgb: "FFFFFF"}
+                },
+                fill: {
+                    fgColor: {rgb: "4472C4"}
+                },
+                alignment: {
+                    horizontal: "center",
+                    vertical: "center"
+                },
+                border: {
+                    top: {style: "thin"},
+                    bottom: {style: "thin"},
+                    left: {style: "thin"},
+                    right: {style: "thin"}
+                }
+            };
+            const dataStyle = {
+                alignment: {
+                    horizontal: "center",
+                    vertical: "center"
+                },
+                border: {
+                    top: {style: "thin"},
+                    bottom: {style: "thin"},
+                    left: {style: "thin"},
+                    right: {style: "thin"}
+                }
+            };
+            const headers = [
+                {v: "요청자", t: "s", s: headerStyle},
+                {v: "승인자", t: "s", s: headerStyle},
+                {v: "상태", t: "s", s: headerStyle},
+                {v: "처리일시", t: "s", s: headerStyle}
+            ];
+
+            const excelData = data.map(item => ([
+                {v: item.요청자이름, t: "s", s: dataStyle},
+                {v: item.승인자이름, t: "s", s: dataStyle},
+                {v: item.요청상태화면명, t: "s", s: dataStyle},
+                {v: item.처리일시, t: "s", s: dataStyle}
+            ]));
+
+            const ws = XLSX.utils.aoa_to_sheet([headers, ...excelData]);
+
+            // 컬럼 넓이 설정
+            ws['!cols'] = [
+                {wch: 15},
+                {wch: 15},
+                {wch: 12},
+                {wch: 15}
+            ];
+            // 워크시트를 워크북에 추가
+            XLSX.utils.book_append_sheet(wb, ws, "외근요청목록");
+
+            // 파일 다운로드
+            XLSX.writeFile(wb, `외근요청목록_${new Date().toISOString().slice(0, 10)}.xlsx`);
+            console.log('Excel 파일 생성 및 다운로드 완료');
+        } catch (error) {
+            console.error('엑셀 다운로드중 에러', error);
+        }
+    }
+    return (
+        <button
+            onClick={excelDownLoad}
+            className="btn btn-primary"
+        >
+            엑셀 다운로드
+        </button>
+    )
+}
+export default ExcelComponent;
